07cbb74547132854b204cc8180a7ab544d000560
Ben Messerly <bamesserly@gmail.com>
2022-12-16 14:45:41 -0600

    Implement test mode.
    Switch modes and n_showers in command line.

--- shower.py
+++ shower.py
@@ -15,7 +15,6 @@ from enum import Enum, auto
 alpha = 0.9 # learning rate
 gamma = 1.0 # discount rate
 epsilon = 0.0 # exploration threshold
-n_showers = 50 # Number of showers to be performed
 mov_list = [-1,0,1]  # Change shower temp
 initial_temp = 38 + random.randint(-3,3)
 shower_length = 60
@@ -44,26 +43,30 @@ action_space = {}
 for i in range(len(mov_list)):
     action_space[i] = (mov_list[i])
 
-# Initialize Q table
-q_table = np.zeros(
-    [len(state_space), len(action_space)]
-)
-#[
-#  [0. 0. 0.]
-#  [0. 0. 0.]
-#    ...
-#  [0. 0. 0.]
-#]
-
-#q_table = np.loadtxt("qtable.csv",delimiter=',') # Uncomment this to read from an existing q table
 
 # Update the q-table after every shower
-def shower(mode = Modes.TRAIN.value):
+def shower(n_showers = 1000, mode = Modes.TRAIN.value):
     try:
         mode = Modes(int(mode))
     except ValueError:
         sys.exit("Invalid mode. Must be either train (0) or test (1).")
     print(mode.name, "mode")
+
+    # Initialize Q table
+    q_table = None
+    if mode == Modes.TRAIN:
+        q_table = np.zeros(
+            [len(state_space), len(action_space)]
+        )
+        #[
+        #  [0. 0. 0.]
+        #  [0. 0. 0.]
+        #    ...
+        #  [0. 0. 0.]
+        #]
+    elif mode == Modes.TEST:
+        q_table = np.loadtxt("qtable.csv",delimiter=',')
+
     # Set local variables
     shower_time = 0
     temp = initial_temp # initialize starting temp for i_shower = 0
@@ -105,20 +108,24 @@ def shower(mode = Modes.TRAIN.value):
             break
 
         # set action
-        if i_shower < 20000000:  # arbitrarily chosen high value to prevent exploitation ;
-            # change above if exploitation is desired afterwards certain number of i_showers
-            temp_change = random.choice(mov_list)
-            action = temp_change+1
-        else:
-            # explore
-            if random.random() < epsilon:
+        if mode == Modes.TRAIN:
+            if i_shower < 20000000:  # arbitrarily chosen high value to prevent exploitation ;
+                # change above if exploitation is desired afterwards certain number of i_showers
                 temp_change = random.choice(mov_list)
                 action = temp_change+1
-            # use q-table ("exploit")
             else:
-                # get the /index/ of the max q-value of the state-th array
-                action = np.argmax(q_table[state])
-                temp_change = action-1
+                # explore
+                if random.random() < epsilon:
+                    temp_change = random.choice(mov_list)
+                    action = temp_change+1
+                # use q-table ("exploit")
+                else:
+                    # get the /index/ of the max q-value of the state-th array
+                    action = np.argmax(q_table[state])
+                    temp_change = action-1
+        elif mode == Modes.TEST:
+            action = np.argmax(q_table[state])
+            temp_change = action-1
 
         # change the action so we don't go out of bounds
         if temp > 74:
@@ -137,25 +144,26 @@ def shower(mode = Modes.TRAIN.value):
             step_reward = -1
 
         # Update the Q table based on this step's reward
-        try:
-            # note about 2d np array access:
-            # q_table[state] accesses the state-th row, which is an array with
-            # length equal to number of actions.
-            # q_table[state, action] access the action-th element of the stateth
-            # array.
-            old_q_value = q_table[state, action]
-        except IndexError:
-            print("ERROR with q_table")
-            print(q_table)
-            print(state, action)
-            sys.exit()
+        if mode == Modes.TRAIN:
+            try:
+                # note about 2d np array access:
+                # q_table[state] accesses the state-th row, which is an array with
+                # length equal to number of actions.
+                # q_table[state, action] access the action-th element of the stateth
+                # array.
+                old_q_value = q_table[state, action]
+            except IndexError:
+                print("ERROR with q_table")
+                print(q_table)
+                print(state, action)
+                sys.exit()
 
-        next_state = temp
-        # max q value given the state after this temp change
-        next_max = np.max(q_table[next_state])
-        q_target = step_reward + gamma * next_max
-        q_delta = q_target - old_q_value
-        q_table[state, action] = old_q_value + alpha * q_delta
+            next_state = temp
+            # max q value given the state after this temp change
+            next_max = np.max(q_table[next_state])
+            q_target = step_reward + gamma * next_max
+            q_delta = q_target - old_q_value
+            q_table[state, action] = old_q_value + alpha * q_delta
 
         # increment shower's reward and time
         shower_reward += step_reward
