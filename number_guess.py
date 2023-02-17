225976ff0b637b29ac3334693fddc32eb0578ebd
Ben Messerly <bamesserly@gmail.com>
2023-02-17 10:07:48 -0600

    Observation states and access functions done.
    Working on the state updating.

--- number_guess.py
+++ number_guess.py
@@ -14,6 +14,27 @@ import math
 import itertools
 
 
+# future answers are -1s -- -1's can't come before real numbers
+#
+# l := np.where(a==-1)[0]           --> indices of the -1s and set it to l
+# if (l := np.where(a==-1)[0]).size --> true if list has a -1 else false
+# a[l[0]:]                          --> from the first -1 until the end ...
+# all(a[l[0]:] == -1)               --> are all -1?
+def has_early_ones(a):
+    return all(a[l[0] :] == -1) if (l := np.where(a == -1)[0]).size else False
+
+
+# numbers in the answer key are unique
+#
+# u               --> list of unique values in a that are greater than 0
+# c               --> corresponding counts of each value in u
+# u[c>1]          --> list of unique values that appear more than once
+# not u[c>1].size --> true when there are no such duplicates
+def answers_are_unique(a):
+    u, c = np.unique(a[a >= 0], return_counts=True)
+    return not u[c > 1].size
+
+
 class NumberGuess(Env):
     def __init__(self, be_verbose=False):
         self.n_numbers = 5
@@ -74,65 +95,77 @@ class NumberGuess2(Env):
 
         # knowledge of the hidden answer: -1 if unknown otherwise contains its value
         # [[-1,-1,-1,-1,-1]...[4,3,2,1,0]]
-        answer_state = np.array([a for a in itertools.product(np.arange(-1,self.n_numbers), repeat=self.n_numbers)])
-
-        # future answers are -1s -- -1's can't come before real numbers
-        #
-        # l := np.where(a==-1)[0]           --> indices of the -1s and set it to l
-        # if (l := np.where(a==-1)[0]).size --> true if list has a -1 else false
-        # a[l[0]:]                          --> from the first -1 until the end ...
-        # all(a[l[0]:] == -1)               --> are all -1?
-        def has_early_ones(a):
-            return all(a[l[0]:] == -1) if (l := np.where(a==-1)[0]).size else False
-
-        # numbers in the answer key are unique
-        #
-        # u               --> list of unique values in a that are greater than 0
-        # c               --> corresponding counts of each value in u
-        # u[c>1]          --> list of unique values that appear more than once
-        # not u[c>1].size --> true when there are no such duplicates
-        def answers_are_unique(a):
-            u,c = np.unique(a[a >= 0], return_counts=True)
-            return not u[c>1].size
-
         # final size of answer space: 1030
+        answer_state = np.array(
+            [
+                a
+                for a in itertools.product(
+                    np.arange(-1, self.n_numbers), repeat=self.n_numbers
+                )
+            ]
+        )
         answer_state = np.array([x for x in answer_state if has_early_ones(x)])
         answer_state = np.array([x for x in answer_state if answers_are_unique(x)])
-
 
         # each number has been guessed or it hasn't
         # [[0,0,0,0,0]...[1,1,1,1,1]]
         # size 160
-        current_guess_state = np.array([a for a in itertools.product(np.arange(0, 2), repeat=self.n_numbers)])
+        current_guess_state = np.array(
+            [a for a in itertools.product(np.arange(0, 2), repeat=self.n_numbers)]
+        )
 
         # concatenate these two state spaces
         # size 65920
-        self.observation_states = np.array([np.concatenate(a) for a in itertools.product(answer_state, current_guess_state)])
+        self.observation_states = np.array(
+            [
+                np.concatenate(a)
+                for a in itertools.product(answer_state, current_guess_state)
+            ]
+        )
 
+        # the environment spaces
+        self.action_space = Discrete(self.n_numbers)
+        self.observation_space = Discrete(self.observation_states.size)
+        self.state = 0  # [-1,-1,-1,-1,-1,0,0,0,0,0] no answers, no guesses yet
+
+        # the answer hidden number sequence
         self.answer = list(range(self.n_numbers))
         random.shuffle(self.answer)
-        self.state = 0  # which index in the answer list are you now?
-        self.action_space = Discrete(self.n_numbers)  # guess any number in the range
-
-        self.observation_space = Discrete(self.observation_states.size)
 
         self.be_verbose = be_verbose
+
         self.n_guesses = 0
-        # self.observation_space = Box(low=np.array([0],dtype=np.uintc), high=np.array([2],dtype=np.uintc))
+
         if self.be_verbose:
             print("NumberGuess::__init__: hidden answer list:", self.answer)
+
+    def get_idx_from_state(self, state):
+        try:
+            return np.where((self.observation_states == state).all(axis=1))[0][0]
+        except IndexError as e:
+            print(
+                "get_idx_from_state Error: state",
+                state,
+                "not found in observation space.",
+            )
+            return -1
+
+    def get_state_from_idx(self, idx):
+        try:
+            return self.observation_states[idx]
+        except IndexError as e:
+            print("get_state_from_idx Error:", e)
+            return np.empty_like(self.observation_states[0])
+
+    # TODO
+    def get_current_guess_idx(self):
+        state = self.observation_states[self.state]
 
     def step(self, action):
         if self.be_verbose:
             print(
-                "current position:",
-                self.state,
-                "/",
-                self.n_numbers - 1,
-                "guess:",
-                action,
-                "correct answer:",
-                self.answer[self.state],
+                f"current position:{self.state}/{self.n_numbers - 1}"
+                "guess:{action},correct answer:{self.answer[self.state]}"
             )
         reward = 0
         if action == self.answer[self.state]:
