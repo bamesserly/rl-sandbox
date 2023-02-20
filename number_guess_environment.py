dcafb1a56d77d22c819fe84bd07fd8ac32ee95fd
Ben Messerly <bamesserly@gmail.com>
2023-02-20 15:56:13 -0600

    Attempt #3 to Solve NumberGuess Game.
    I reduced the observation space by a factor of 100
    because I realized ordering doesn't matter.
    
    But it's still blundering and playing almost randomly.

--- number_guess_environment.py
+++ number_guess_environment.py
@@ -249,6 +249,160 @@ class NumberGuess2(Env):
         return self.state
 
 
+class NumberGuess3(Env):
+    def __init__(self, be_verbose=False):
+        self.n_numbers = 5
+
+        # current knowledge of the hidden answer sequence:
+        # we get all the info we need from just an unordered list of the
+        # previous correctly-guessed numbers.
+        # [0,-1,-1,-1,-1],[1,-1,-1,-1,-1],[2,-1,-1,-1,-1],...,
+        # [0,1,-1,-1,-1],[0,2,-1,-1,-1],..., [0,1,2,3,4]
+        # This brings us down to just 32 entries.
+        answer_state = []
+        for i in range(self.n_numbers+1):
+            for x in list(itertools.combinations(range(self.n_numbers),i)):
+                answer_state.extend([list(x) +[-1]*(self.n_numbers-len(x))])
+        answer_state = np.array(answer_state)
+        # [list(x) +[-1]*(4-len(x)) for x in list(itertools.combinations(range(5),2))]
+
+        # each number has been guessed or it hasn't
+        # [[0,0,0,0,0]...[1,1,1,1,1]]
+        # size 32
+        current_guess_state = np.array(
+            [a for a in itertools.product(np.arange(0, 2), repeat=self.n_numbers)]
+        )
+
+        # concatenate these two state spaces
+        # size 1024
+        self.observation_states = np.array(
+            [
+                np.concatenate(a)
+                for a in itertools.product(answer_state, current_guess_state)
+            ]
+        )
+
+        # the environment spaces
+        self.action_space = Discrete(self.n_numbers)
+        self.observation_space = Discrete(self.observation_states.shape[0])
+        self.state = 0  # [-1,-1,-1,-1,-1,0,0,0,0,0] no answers, no guesses yet
+
+        # the answer hidden number sequence
+        self.answer = list(range(self.n_numbers))
+        random.shuffle(self.answer)
+
+        self.be_verbose = be_verbose
+
+        self.n_guesses = 0
+
+        if self.be_verbose:
+            print("NumberGuess::__init__: hidden answer list:", self.answer)
+
+    # in: array element of self.observation_states
+    # out: int element of self.observation_space
+    def get_state_from_array(self, arr):
+        try:
+            return np.where((self.observation_states == arr).all(axis=1))[0][0]
+        except IndexError as e:
+            print(
+                "get_state_from_array Error: array",
+                arr,
+                "not found in observation space.",
+            )
+            return -1
+
+    # in: int element of self.observation_space
+    # out: array element of self.observation_states
+    def get_array_from_state(self, state = None):
+        state = state if state != None else self.state
+        try:
+            return self.observation_states[state]
+        except IndexError as e:
+            print("get_array_from_state Error:", e)
+            return np.empty_like(self.observation_states[0])
+
+    # in: int element of self.observation_space
+    # out: human-friendly dict
+    def get_dict_from_state(self, state = None):
+        keys = ["answer0", "answer1", "answer2", "answer3","answer4",
+                "guess0", "guess1", "guess2", "guess3", "guess4"]
+        state = state if state != None else self.state
+        arr = self.get_array_from_state(state)
+        ret = dict(zip(keys, list(arr)))
+        try:
+            ret["current_position"] = list(arr).index(next((x for x in arr if x == -1), None))
+        # we have already correctly guessed the final answer
+        except ValueError:
+            ret["current_position"] = 4
+        return ret
+
+    # in: human-friendly dict
+    # out: int element of self.observation_space
+    def get_state_from_dict(self, d):
+        ret = np.array([val for key, val in d.items])
+        assert self.get_state_from_array(ret)
+        return ret
+
+
+    def get_new_state(self, action):
+        initial_position = self.get_dict_from_state()["current_position"]
+        final_state_array = np.copy(self.get_array_from_state())
+
+        # if guess is correct
+        if action == self.answer[initial_position]:
+            # update knowledge of answer
+            final_state_array[initial_position] = action
+            final_state_array[:initial_position+1].sort()
+            # reset current guesses all to -1
+            final_state_array[self.n_numbers:].fill(0)
+        # if guess is incorrect
+        else:
+            # update guess
+            final_state_array[self.n_numbers + action] = 1
+
+        return self.get_state_from_array(final_state_array)
+
+
+    def is_done(self):
+        ret = -1 not in self.get_array_from_state()
+        return ret
+
+
+    def step(self, action):
+        self.n_guesses += 1
+        initial_position = self.get_dict_from_state()["current_position"]
+        self.state = self.get_new_state(action)
+        final_position = self.get_dict_from_state()["current_position"]
+        self.n_guesses += 1
+        if self.be_verbose:
+            print(
+                f"current position: {initial_position}/{self.n_numbers - 1} | "
+                f"guess:{action} | correct answer:{self.answer[initial_position]}"
+            )
+        reward = 1 if final_position > initial_position else - 1
+
+        if reward == 1:
+            self.n_guesses = 0
+
+        done = self.is_done()
+
+        info = self.get_dict_from_state()
+
+        # Return step information
+        return self.state, reward, done, info
+
+    def render(self):
+        pass
+
+    def reset(self):
+        random.shuffle(self.answer)
+        self.state = 0
+        self.n_guesses = 0
+        if self.be_verbose:
+            print("NumberGuess::reset: hidden answer list:", self.answer)
+        return self.state
+
+
 if __name__ == "__main__":
     env = NumberGuess()
     # print(env.observation_space.sample()) # 0-1
